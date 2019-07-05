37cdafa113e1e40bbe7790b56abc539f72eeda87
kyle-manganyi <tmanganyi@retrorabbit.co.za>
2019-07-05 12:49:28 +0200

    updated tests

--- tangent_solutions/test.py
+++ tangent_solutions/test.py
@@ -9,15 +9,15 @@ class empty_leave_request(unittest.TestCase):
         self.assertEqual(response.status_code, 500)
 
 
-class create_leave_test(unittest.TestCase):
-    def test_empty_post(self):
+class invalid_employee(unittest.TestCase):
+    def test_empty_incorrect_post(self):
         response = requests.post('http://localhost:5000/leave', json={
             "emp_number": "2",
             "start_date": "2011-01-03",
             "end_date": "2012-01-03",
             "days_of_leave": 10
         })
-        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b'Employee Does not exist' in response)
 
 
 if __name__ == "__main__":
